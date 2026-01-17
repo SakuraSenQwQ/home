85bad31900898f06b13735871464ec89784a68a0
SomeOvO <im@sakurasen.cn>
2026-01-17 20:02:46 +0800

    Refactor code structure for improved readability and maintainability

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <script lang="ts" setup>
 import c_Music from './page/c_Music.vue';
 const rand = Math.floor(Math.random() * 5)
+if (window.location.pathname == '/icon'){
+  window.location.pathname = '/imgs/avat.png'
+}
 </script>
 
 <template>
