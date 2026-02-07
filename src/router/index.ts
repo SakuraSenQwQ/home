38846128043f1a26658c724b8e6f2e4eaa00f8fc
SomeOvO <im@sakurasen.cn>
2026-02-08 01:03:39 +0800

    feat: 添加人机验证组件 V_captcha

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import V_main from '@/view/V_main.vue'
 import V_sponsor from '@/view/V_sponsor.vue'
+import V_captcha from '@/view/V_captcha.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [{path:'/',component:V_main},{path:'/sponsor',component:V_sponsor}],
+  routes: [{path:'/',component:V_main},{path:'/sponsor',component:V_sponsor},{path:"/captcha",component:V_captcha}],
   scrollBehavior(){
     return {top:0,left:0}
   }
---
+++ src/view/V_captcha.vue
@@ -0,0 +1,121 @@
+<script lang="ts" setup>
+  import { ref } from 'vue'
+
+  const url = window.location.href
+  const u = new URL(url)
+  const gt = u.searchParams.get("a")
+  const cl = u.searchParams.get("b")
+  console.log(gt, cl)
+  const p = ref("加载中")
+  const res = ref(null)
+  const ok = ref(false)
+  function cp(){
+    navigator.clipboard.writeText("#终末地 人机验证 "+JSON.stringify(res.value))
+  }
+  if (gt == null || cl == null) {
+    p.value = "参数错误！"
+    alert(p.value)
+  } else {
+    const sc = document.createElement("script")
+    sc.src = "https://static.geetest.com/static/js/gt.0.5.0.js"
+    document.body.append(sc)
+    sc.addEventListener("load", () => {
+      const captchaBox = document.getElementById("captcha-box");
+
+      initGeetest(
+
+        {
+
+          // 以下配置参数来自服务端 SDK
+
+          gt: gt,
+
+          challenge: cl,
+
+          offline: false,
+
+          new_captcha: true,
+
+          api_server: "api.geevisit.com", //非必须参数，添加此字段用于修改极验api域名
+
+        },
+
+        function (captchaObj) {
+          captchaObj.appendTo(captchaBox);
+          captchaObj.onReady(function () {
+            // DOM 准备好后，隐藏 #loading-tip 元素
+            // 仅作示例用，用您适合的方式隐藏即可
+            p.value = "请进行人机验证"
+          });
+          captchaObj.onSuccess(function () {
+
+            const result = captchaObj.getValidate();
+
+            console.log(result)
+            res.value = result
+            ok.value = true
+
+          });
+          captchaObj.onError(function (error) {
+            // 出错啦，可以提醒用户稍后进行重试
+            // error 包含error_code、msg
+            p.value = "加载错误"
+            console.log(error.msg)
+          });
+
+        },
+
+      );
+    })
+  }
+</script>
+
+<template>
+<div class="main page">
+
+  <div class="box ">
+    <h1>人机验证</h1>
+    <p>{{ p }}</p>
+    <div id="captcha-box"></div>
+    <div class="scus" v-if="ok">
+      <p>认证通过</p>
+      <p>请复制以下内容</p>
+      <span @click="cp()">点击复制</span>
+      <p>{{ "#终末地 人机验证 "+JSON.stringify(res) }}</p>
+    </div>
+  </div>
+
+</div>
+</template>
+
+<style scoped>
+.scus{
+  margin-top: 1rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  max-width: 100%;
+  word-break: break-all;
+}
+#captcha-box {
+  display: flex;
+  justify-content: center;
+}
+
+.box {
+  display: flex;
+  flex-direction: column;
+
+  justify-content: center;
+
+  h1,
+  p {
+    color: #fff;
+    white-space: wrap;
+  }
+  span{
+    color: #aeff6d;
+  }
+}
+</style>
